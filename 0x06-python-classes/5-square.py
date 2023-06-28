335d4e00f112d1dd494b0c92579bca3c3ef58bb3
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-06-28 17:26:20 +0100

    Class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+
+""" Creation of the Square class """
+
+
+class Square:
+
+    """ Definition of the Square class """
+    __size = None
+
+    def __init__(self, size=0):
+        """ Initialisation of the class """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Initialisation of the function area """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ Initialisation of the function getter for size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Initialisation of the function setter for size """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    def my_print(self):
+        """ Initialisation of the function my_print """
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
