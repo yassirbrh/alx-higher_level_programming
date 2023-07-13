593bcba764ade480243b084a94d98a82b0e5cc51
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-07-13 20:24:26 +0100

    Update the class Rectangle: adding the method update to update the instance attributes.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -135,3 +135,18 @@ class Rectangle(Base):
         string += str(self.x) + "/" + str(self.y) + " - "
         string += str(self.width) + "/" + str(self.height)
         return string
+
+    def update(self, *args):
+        '''
+            Function to update the object properties
+        '''
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
