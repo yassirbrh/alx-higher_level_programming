a3e805de1021a1e6e4dbc3a5d897c9f27f5b42f2
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-07-14 12:58:09 +0100

    Update the Square class: adding the update instance method to update the instance attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,14 +15,13 @@ class Square(Rectangle):
             Initialisation of the constructor of the class Square
         '''
         super().__init__(size, size, x, y, id)
-        self.__size = size
 
     @property
     def size(self):
         '''
             Initialisation of the size getter
         '''
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -33,7 +32,8 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         '''
@@ -43,3 +43,27 @@ class Square(Rectangle):
         string += str(self.x) + "/" + str(self.y) + " - "
         string += str(self.width)
         return string
+
+    def update(self, *args, **kwargs):
+        '''
+            Initialisation of the instance method update
+        '''
+        if args is not None and len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for kwarg in kwargs:
+                if kwarg == "id":
+                    self.id = kwargs[kwarg]
+                if kwarg == "size":
+                    self.size = kwargs[kwarg]
+                if kwarg == "x":
+                    self.x = kwargs[kwarg]
+                if kwarg == "y":
+                    self.y = kwargs[kwarg]
