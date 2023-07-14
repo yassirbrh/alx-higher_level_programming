54d7360e26e0f312d497d827b79ee55bd8ffdd1f
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-07-14 12:38:17 +0100

    Update the Square class: Adding the size property setter and getter.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,6 +15,25 @@ class Square(Rectangle):
             Initialisation of the constructor of the class Square
         '''
         super().__init__(size, size, x, y, id)
+        self.__size = size
+
+    @property
+    def size(self):
+        '''
+            Initialisation of the size getter
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''
+            Initialisation of the size setter
+        '''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = value
 
     def __str__(self):
         '''
