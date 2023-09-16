e02cd2d10673ab11617129c49cd4d538035b81df
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-09-16 15:17:21 +0100

    Script that lists all State objects from the database hbtn_0e_6_usa.

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+    Script that lists all State objects from the database hbtn_0e_6_usa
+'''
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import State, Base
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(usr, pwd, db),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    first_row = session.query(State).first()
+    if first_row:
+        print("{}: {}".format(first_row.id, first_row.name))
+    else:
+        print()
+    session.close()
