ba8430e71a0918a62674536ce8bf79dcb2505aab
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-09-16 10:36:48 +0100

    Script that lists all cities from the database hbtn_0e_4_usa.

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute(
         """SELECT cities.name FROM cities
-        JOIN states ON states.id = cities.state_id
+        INNER JOIN states ON states.id = cities.state_id
         WHERE states.name LIKE %s ORDER BY cities.id ASC""", (sys.argv[4], ))
     query_rows = cur.fetchall()
     for row in query_rows:
