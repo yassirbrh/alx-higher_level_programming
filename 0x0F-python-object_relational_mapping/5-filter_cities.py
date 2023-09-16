ce2a14878ff6bf01d963e0a668bc513e42d87ddf
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-09-16 10:29:53 +0100

    Script that lists all cities from the database hbtn_0e_4_usa.

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+    Script that lists all cities from the database hbtn_0e_4_usa.
+'''
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306,
+        charset="utf8"
+        )
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT cities.name FROM cities
+        JOIN states ON states.id = cities.state_id
+        WHERE states.name LIKE %s""", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row != query_rows[len(query_rows) - 1]:
+            print(row[0], end=", ")
+        else:
+            print(row[0])
+    cur.close()
+    conn.close()
