dc802644ffed0d52f2c13cafbb780d81fae9b0ed
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-09-29 13:19:12 +0100

    Script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     with request.urlopen(url) as response:
         data = response.read()
         print("Body response:")
-        print("\t- type: ", type(data))
-        print("\t- content: ", data)
-        print("\t- utf8 content: ", data.decode('utf-8'))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))
