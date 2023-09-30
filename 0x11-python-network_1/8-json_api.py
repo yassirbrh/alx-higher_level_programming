7d87d9eda1e3cd298be3b6feaf6549aab0657028
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-09-30 17:33:03 +0100

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+'''
+    Python script that takes in a letter
+    and sends a POST request to http://0.0.0.0:5000/search_user
+    with the letter as a parameter.
+'''
+
+if __name__ == "__main__":
+    '''
+        Importing the requests library
+        and sys.argv
+    '''
+    import requests
+    from sys import argv
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': argv[1]}
+    request = requests.post(url, data)
+    try:
+        usr_id = request.json().get("id")
+        name = request.json().get("name")
+        if request.json():
+            print("[{}] {}".format(usr_id, name))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
