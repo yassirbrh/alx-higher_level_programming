ef4089e3032313caea91829a333873d373a49953
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-10-24 18:40:19 +0100

    Script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const content = process.argv[3];
+fs.writeFile(process.argv[2], content, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
