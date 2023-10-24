29cf0aaf6c50258a9126f2a61618b1d03134d92f
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-10-24 20:05:53 +0100

    Script that display the status code of a GET Request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error('error: ', error);
+  }
+  else {
+    console.log('code: ', response.statusCode);
+  }
+});
