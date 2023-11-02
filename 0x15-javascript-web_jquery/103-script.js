ed5fe67fda2cf5efd285e0eb1d039d073222acee
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-11-02 01:02:07 +0000

    Add task 12 and 13.

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,16 @@
+$('document').ready(function () {
+  $('INPUT#btn_translate').click(translate);
+  $('INPUT#language_code').focus(function () {
+    $(this).keydown(function (e) {
+      if (e.keyCode === 13) {
+        translate();
+      }
+    });
+  });
+});
+function translate () {
+  const link = 'https://www.fourtonfish.com/hellosalut/?';
+  $.get(link + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+    $('DIV#hello').html(data.hello);
+  });
+}
